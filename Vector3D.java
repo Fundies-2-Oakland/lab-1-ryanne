2434d9a6b1dcdea93501c0fee5c3a36ca4d336d4
rymakena <rymakena208@gmail.com>
2025-01-10 12:08:31 -0800

    Add files via upload

---
+++ Vector3D.java
@@ -0,0 +1,32 @@
+import static java.lang.Math.sqrt;
+
+public class Vector3D {
+
+    double x, y, z;
+
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ',' + y + ',' + z + ')';
+    }
+
+    public void main() {
+    }
+}
